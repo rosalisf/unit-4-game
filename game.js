5950d2606bb92b12bf2685b84cb1d27b56f7b0db
Rosalis Freebairn <rfreeb@artic.edu>
2019-08-17 13:42:22 -0500

    Basic styling added to header and instructions.

--- game.js
+++ game.js
@@ -1,1 +1,33 @@
-let randonNumber = 19 - 120;
+// Set the random number values
+
+// Make gems clickable and give them a random value
+
+// This makes a button that counts how many times you click it
+
+// $('#container').html('<h1>hello</h1>')
+
+// var counter = 0
+
+// event listener for 4 buttons
+// $('.good-button').on('click', function(){
+//  var randomValue = parseInt($(this).attr('randomValue'))
+//  counter += randomValue
+//  $('#container').html(counter)
+// })
+
+// This makes a button for each crystal count by their number value
+
+// var crystals = ['red', 'green', 'yellow', 'blue']
+
+// creates 4 new crystals
+// for (let i = 0; i < crystals.length; i++) {
+//  var newButton = $('<button>')
+//  newButton.text(crystals[i])
+//  newButton.addClass('good-button')
+//  newButton.attr('randomvalue', Math.ceil(Math.random() * 12))
+//  $('#container').append(newButton)
+// }
+
+// Have a set goal everytime the game ends/resets
+
+// Have the number of wins and losses recorded on the page
