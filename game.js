9a500b6efe3203549ff2fe3daf2369800374bde7
Rosalis Freebairn <rfreeb@artic.edu>
2019-08-17 15:06:27 -0500

    Gems will show up as clickable buttons and have a hidden value.

--- game.js
+++ game.js
@@ -6,27 +6,42 @@
 
 // $('#container').html('<h1>hello</h1>')
 
-// var counter = 0
+var counter = 0;
 
 // event listener for 4 buttons
-// $('.good-button').on('click', function(){
-//  var randomValue = parseInt($(this).attr('randomValue'))
-//  counter += randomValue
-//  $('#container').html(counter)
-// })
+$(document).on("click", ".gem", function() {
+  console.log("working");
+  var randomValue = parseInt($(this).attr("randomValue"));
+  counter += randomValue;
+  $("#counter").html(counter);
+});
 
 // This makes a button for each crystal count by their number value
 
-// var crystals = ['red', 'green', 'yellow', 'blue']
+var crystals = ["pink", "green", "yellow", "blue"];
 
 // creates 4 new crystals
-// for (let i = 0; i < crystals.length; i++) {
-//  var newButton = $('<button>')
-//  newButton.text(crystals[i])
-//  newButton.addClass('good-button')
-//  newButton.attr('randomvalue', Math.ceil(Math.random() * 12))
-//  $('#container').append(newButton)
-// }
+for (let i = 0; i < crystals.length; i++) {
+  var newButton = $("<img>");
+  newButton.addClass("gem");
+  newButton.attr("randomvalue", Math.ceil(Math.random() * 12));
+  if (crystals[i] === "green") {
+    newButton.attr("src", "images/green_gem.gif");
+  }
+
+  if (crystals[i] === "pink") {
+    newButton.attr("src", "images/pink_gem.gif");
+  }
+
+  if (crystals[i] === "yellow") {
+    newButton.attr("src", "images/yellow_gem.gif");
+  }
+
+  if (crystals[i] === "blue") {
+    newButton.attr("src", "images/blue_gem.gif");
+  }
+  $("#container").append(newButton);
+}
 
 // Have a set goal everytime the game ends/resets
 
